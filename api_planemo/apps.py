0a88616bcbbb7356691b31bb1d5e8ac57654da7c
Eric Rasche <rasche.eric@gmail.com>
2015-12-23 11:53:16 -0600

    Import the base/api_planemo

---
+++ api_planemo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiPlanemoConfig(AppConfig):
+    name = 'api_planemo'
--- shed/settings.py
+++ shed/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
+    'api_planemo',
 ]
 
 MIDDLEWARE_CLASSES = [
