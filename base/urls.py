35938c6275f749592a556c0114c0c2ce4981d9c5
Eric Rasche <rasche.eric@gmail.com>
2015-12-23 12:53:02 -0600

    trailing slash is needed

--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,8 @@ from api_drf import urls as api_drf_urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'api/drf', include(api_drf_urls)),
-    url(r'api/planemo', include(api_planemo_urls)),
+    url(r'api/drf/', include(api_drf_urls)),
+    url(r'api/planemo/', include(api_planemo_urls)),
+
     url(r'api/', api_list, name='api_list'),
 ]
