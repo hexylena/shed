205274940e185fca28616e2d8c9a95027e7b37e9
Eric Rasche <rasche.eric@gmail.com>
2016-01-01 14:39:44 -0600

    Remove trailing slashes

--- api_ts_old/urls.py
+++ api_ts_old/urls.py
@@ -13,14 +13,14 @@ urlpatterns = [
 
     url(r'v1/api/authenticate/baseauth$', v1_baseauth),
 
-    url(r'v1/api/users/$', v1_user_list, name='api-planemo-user-list'),
+    url(r'v1/api/users$', v1_user_list, name='api-planemo-user-list'),
     url(r'v1/api/users/' + PK, v1_user_detail, name='api-planemo-user-detail'),
 
-    url(r'v1/api/categories/$', v1_cat_list, name='api-planemo-cat-list'),
-    url(r'v1/api/categories/' + PK + '/$', v1_cat_detail, name='api-planemo-cat-detail'),
+    url(r'v1/api/categories$', v1_cat_list, name='api-planemo-cat-list'),
+    url(r'v1/api/categories/' + PK + '$', v1_cat_detail, name='api-planemo-cat-detail'),
     url(r'v1/api/repository/download/', v1_download),
 
-    url(r'v1/api/repositories/$', v1_repo_list, name='api-planemo-repo-list'),
-    url(r'v1/api/repositories/' + PK + '/$', v1_repo_detail),
-    url(r'v1/api/repositories/' + PK + '/changeset_revision/' + PK2, v1_rev_detail),
+    url(r'v1/api/repositories$', v1_repo_list, name='api-planemo-repo-list'),
+    url(r'v1/api/repositories/' + PK + '$', v1_repo_detail),
+    url(r'v1/api/repositories/' + PK + '/changeset_revision' + PK2, v1_rev_detail),
 ]
